b74cc0d0b11265190d257ad131e9dfd10019d30d
anylots <842567494@qq.com>
2023-06-26 17:40:55 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use ethers::types::{Address};
 use std::{error::Error, str::FromStr, sync::Arc};
 
 const CONTRACT_ADDRESS: &str = "";
-const PRIVATE_KEY: &str = "0xebd24eb9345e93582be65937c87932f8b7e97c80c4d51e83b1e2bdc33c4acc42";
+const PRIVATE_KEY: &str = "ebd24eb9345e93582be65937c87932f8b7e97c80c4d51e83b1e2bdc33c4acc42";
 #[tokio::main]
 
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -25,7 +25,7 @@ async fn call() -> Result<(), Box<dyn Error>> {
 
     let signer = Arc::new(SignerMiddleware::new(
         provider.clone(),
-        wallet.with_chain_id(53077 as u64),
+        wallet.with_chain_id(9000 as u64),
     ));
 
     abigen!(TestZkEVM, "./resource/abi/TestZkEVM.json");
@@ -36,7 +36,7 @@ async fn call() -> Result<(), Box<dyn Error>> {
     let tx = testZkEVM.transfer(
         Address::from_str("0xa210b31C70737AA2E09A0fFC151CF21e18365954").unwrap(),
         10.into(),
-    );
+    ).legacy();
     let receipt = tx.send().await;
     match receipt {
         Ok(sent_tx) => println!("====transaction ID: {:?}", sent_tx),
@@ -51,15 +51,18 @@ async fn deploy() -> Result<(), Box<dyn Error>> {
 
     let signer = Arc::new(SignerMiddleware::new(
         provider.clone(),
-        wallet.with_chain_id(53077 as u64),
+        wallet.with_chain_id(9000 as u64),
     ));
 
     abigen!(TestZkEVM, "./resource/abi/TestZkEVM.json");
     let a: u64 = 10;
-    let testZkEVM = TestZkEVM::deploy(signer, a.pow(18))
-        .unwrap()
-        .send()
-        .await;
+    // let testZkEVM = TestZkEVM::deploy(signer, a.pow(18)) .nonce(1)
+    //     .unwrap()
+    //     .send()
+    //     .await;
+
+    let tx = TestZkEVM::deploy(signer, a.pow(18))?.legacy();
+    let testZkEVM = tx.send().await;
 
     match testZkEVM {
         Ok(sent_tx) => println!("====testZkEVM: {:?}", sent_tx),
